7f88d04fed90988dca70f9682ebb3602cb1f8956
Mo Zhu <79839921+zzmmzzm@users.noreply.github.com>
2024-12-26 19:31:08 +0800

    Update main.py

--- main.py
+++ main.py
@@ -191,7 +191,7 @@ if __name__ == "__main__":
         raise ValueError(
             "OpenAI key is not set - please set OAI_KEY to your OpenAI key"
         )
-    openai_client = OpenAI(api_key=OAI_KEY,base_url="https://api.deepseek.com/v1")
+    openai_client = OpenAI(api_key=OAI_KEY, base_url="https://api.deepseek.com")
     # load the author list
     with io.open("configs/authors.txt", "r") as fopen:
         author_names, author_ids = parse_authors(fopen.readlines())
